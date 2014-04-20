a74330ca79da5f22c0f1ecf16f3887afeff3c17f
Dimitar Bonev <dsbonev@gmail.com>
2014-04-21 00:33:35 +0300

    style article views

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ News::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
