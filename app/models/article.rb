49b14a5c38eca3a8c3bd53374d69e80b02f9a5fb
Dimitar Bonev <dsbonev@gmail.com>
2014-04-21 23:23:51 +0300

    sort Articles by :created_at DESC

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  default_scope { order 'created_at DESC' }
   belongs_to :author
   validates_presence_of :title, :summary, :body, :author
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should 'be ordered by :created_at DESC' do
+    create_list :article, 3
+    articles = Article.all
+    assert_equal 3, articles.count
+
+    todays = articles[0].tap { |a| a.update_attributes! created_at: Date.today }
+    tomorrows = articles[1].tap { |a| a.update_attributes! created_at: Date.tomorrow }
+    yesterdays = articles[2].tap { |a| a.update_attributes! created_at: Date.yesterday }
+
+    assert_equal [tomorrows, todays, yesterdays].map(&:created_at), Article.all.map(&:created_at)
+  end
 end
