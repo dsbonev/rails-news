018c91cfe17494022a52a3396968e0ffdd62d0d9
Dimitar Bonev <dsbonev@gmail.com>
2014-04-21 16:17:35 +0300

    assert Author destroyes own articles

--- test/factories/authors.rb
+++ test/factories/authors.rb
@@ -3,5 +3,15 @@
 FactoryGirl.define do
   factory :author do
     name { Faker::Name.name }
+
+    factory :author_with_articles do
+      ignore do
+        article_count 2
+      end
+
+      after(:create) do |author, evaluator|
+        create_list(:article, evaluator.article_count, author: author)
+      end
+    end
   end
 end
--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -6,8 +6,22 @@ class AuthorTest < ActiveSupport::TestCase
       author = create :author
       duplicate_author = build :author,
                                 name: author.name.swapcase
+
       assert duplicate_author.invalid?
+
       assert duplicate_author.errors[:name].present?
+    end
+  end
+
+  context '#destroy' do
+    should 'destroy own articles too' do
+      author = create :author_with_articles
+      articles = author.articles
+      assert articles.present?
+
+      author.destroy
+
+      assert articles.all? &:destroyed?
     end
   end
 end
